be1b714244d616216f950154c602f341f2637470
alfamegaxq <zarokas@gmail.com>
2014-09-18 22:25:30 +0300

    tile generation
    generating tiles for map. first try

---
+++ Assets/generation/MapGeneration.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGeneration : MonoBehaviour {
+	private const int MAP_SIZE_X = 50;
+	private const int MAP_SIZE_Y = 50;
+
+	void Start () {
+		float nextPositionX = 0;
+		float nextPositionY = 0;
+		for(int x = 0; x < MAP_SIZE_X; x++){
+			for(int y = 0; y < MAP_SIZE_Y; y++){
+				GameObject tile = GameObject.CreatePrimitive(PrimitiveType.Plane);
+				tile.transform.position = new Vector3(x + nextPositionX, y + nextPositionY, 0);
+				tile.transform.localScale = new Vector3 (0.1f, 0.1f, 0.1f);
+				tile.transform.Rotate (new Vector3 (-90, 0, 0));
+				tile.AddComponent ("Tile");
+				tile.name = "tile";
+				nextPositionY += 0.1f;
+			}
+			nextPositionX += 0.1f;
+			nextPositionY = 0;
+		}
+
+	}
+
+	void Update () {
+	
+	}
+}
---
+++ Assets/generation/details/tiles/Tile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class Tile : MonoBehaviour{
+	private enum BASE_LEVELS {none, green, yellow, red};
+
+	public int x { get; set; }
+	public int y { get; set; }
+	public int base_level { get; set; }
+	public int soldiers { get; set; }
+	public int generating_soldiers_in_hour { get; set; }
+	public DateTime soldiers_arriving { get; set; }
+	public DateTime battle_start { get; set; }
+
+}
